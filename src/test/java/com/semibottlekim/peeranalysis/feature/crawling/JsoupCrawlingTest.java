04fe1abe841fa21839b44fdc729df1039a101206
Kim Byung Jun <celbever@naver.com>
2022-03-24 16:41:18 +0900

    [crawling] jsoup 캐릭터 이름 추출 테스트

--- src/test/java/com/semibottlekim/peeranalysis/feature/crawling/JsoupCrawlingTest.java
+++ src/test/java/com/semibottlekim/peeranalysis/feature/crawling/JsoupCrawlingTest.java
@@ -1,7 +1,10 @@
 package com.semibottlekim.peeranalysis.feature.crawling;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -25,4 +28,19 @@ public class JsoupCrawlingTest {
         assertThat(doc.title()).isEqualTo(title);
     }
 
+    @Test
+    public void crawling시_성공적으로_캐릭터이름을_가져올수있어야한다() throws IOException {
+        //given
+        String url = "https://lostark.game.onstove.com/Profile/Character/%EC%97%90%ED%94%8C%EB%A6%BF";
+        String targetDivClass = "profile-character-info__name";
+        String characterName = "에플릿";
+
+        //when
+        Document doc = Jsoup.connect(url).get();
+
+        //then
+        Elements target = doc.body().getElementsByClass(targetDivClass);
+        String result = target.attr("title");
+        assertThat(result).isEqualTo(characterName);
+    }
 }
